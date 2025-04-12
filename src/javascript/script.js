b8358c1c20ef4d41efcf5f781a8a0e3c6bbaee21
SouzaStack <193841372+SouzaStack@users.noreply.github.com>
2025-04-11 22:23:47 -0300

    feat: Links do menu destacam seções

---
+++ src/javascript/script.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+   const mapLinks = {
+     "Figure": "area-figure",
+     "Picture": "area-picture",
+     "Audio": "area-audio",
+     "Video": "area-video"
+   };
+
+   const links = document.querySelectorAll("menu ul li a");
+
+   links.forEach(link => {
+     link.addEventListener("click", (e) => {
+       e.preventDefault();
+
+       const linkText = link.textContent.trim(); // Pega o texto clicado
+       const targetId = mapLinks[linkText]; // Usa o texto pra achar o id
+       const targetElement = document.getElementById(targetId); // Seleciona o elemento correspondente
+
+       if (targetElement) {
+         targetElement.style.backgroundColor = "#fef08a"; // amarelo destaque
+
+         setTimeout(() => {
+           targetElement.style.backgroundColor = ""; // volta ao normal depois de 5s
+         }, 500);
+       }
+     });
+   });
+};
